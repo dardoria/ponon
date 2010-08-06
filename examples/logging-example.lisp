8fc7452c763a82665989ea21a556f30cfef63214
Boian Tzonev <boiantz@gmail.com>
2010-08-06 16:38:56 +0300

    Add licensing info. License is MIT.

--- examples/logging-example.lisp
+++ examples/logging-example.lisp
@@ -1,3 +1,27 @@
+;;; -*- Mode: Lisp; indent-tabs-mode: nil -*-
+;;;
+;;; loggin-example.lisp --- A simple app that logs the invocation of every app method
+;;;
+;;; Copyright (c) 2010, Boian Tzonev <boiantz@gmail.com>
+;;;
+;;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;;; of this software and associated documentation files (the "Software"), to deal
+;;; in the Software without restriction, including without limitation the rights
+;;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;;; copies of the Software, and to permit persons to whom the Software is
+;;; furnished to do so, subject to the following conditions:
+;;;
+;;; The above copyright notice and this permission notice shall be included in
+;;; all copies or substantial portions of the Software.
+;;;
+;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+;;; THE SOFTWARE.
+
 (defpackage :ponon-logging-example
   (:use :cl :ponon))
 
@@ -8,7 +32,8 @@
 (defmethod setup ((app logging-app))
   (format t "~a~%" "setup logging app"))
 
-(defmethod update ((app logging-app)))
+(defmethod update ((app logging-app))
+  (format t "~a~%" "update logging app"))
 
 (defmethod draw ((app logging-app))
   (format t "~a~%" "draw logging app"))
